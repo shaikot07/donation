78e406001e7f88a3edb90299e445e19e0ed6671f
shaikot07 <saifulislamshaikat007@gmail.com>
2023-09-26 16:23:59 +0600

    fix some issus

--- src/Component/Error/ErrorPage.jsx
+++ src/Component/Error/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
       return (
             <div className='text-center mt-60'>
                   <h2 className='text-red-600 text-4xl text-center'>Oooops!!</h2>
-                  <h2 className='text-red-600 text-2xl text-center'>Data not Found</h2>
+                  <h2 className='text-red-600 text-2xl text-center'>page not Found</h2>
                   <Link to='/'><button className='bg-red-600 text-white px-5 py-2 rounded-md mt-5'>Go back Home</button></Link>
             </div>
       );
